8bc5bc65c6eca8187123a6b89511723c22e2419b
Dimitri NSD <it@tenteeglobal.com>
2024-02-14 15:46:05 +0100

    fixing deployment bugs (14)

--- myapp/views.py
+++ myapp/views.py
@@ -243,19 +243,20 @@ def google_analytics_check(soup):
 
 def page_load_time(url):
 
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_argument('--headless')
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
+
     # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
     # driver = webdriver.Chrome()
 
-    # options.binary_location = '/usr/bin/google-chrome'
-    # driver = webdriver.Chrome(service=ChromeService('/usr/bin/chromedriver'), options=options)
+    options.binary_location = '/usr/bin/google-chrome'
+    driver = webdriver.Chrome(service=ChromeService('/usr/bin/chromedriver'), options=options)
 
-    folder = pathlib.Path(__file__).parent.resolve()
-    chrome_driver_path = str(folder) + "\chromedriver.exe"
-    driver = webdriver.Chrome(chrome_driver_path, options=options)
+    # folder = pathlib.Path(__file__).parent.resolve()
+    # chrome_driver_path = str(folder) + "\chromedriver.exe"
+    # driver = webdriver.Chrome(chrome_driver_path, options=options)
 
     driver.get(url)
     # navigationStart = driver.execute_script("return window.performance.timing.navigationStart")
