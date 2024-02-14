51cb1e44d28f0e39bb968a95a067228bf122465b
Dimitri NSD <it@tenteeglobal.com>
2024-02-14 16:03:12 +0100

    fixing deployment bugs (16)

--- myapp/views.py
+++ myapp/views.py
@@ -16,13 +16,13 @@ from webdriver_manager.chrome import ChromeDriverManager
 
 def data(url):
     page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
+    soup = BeautifulSoup(page.content, "html.parser")
     return soup
 
 
 def title(soup):
-    message = 'No Title Found'
-    text = soup.find('title').getText()
+    message = "No Title Found"
+    text = soup.find("title").getText()
     if len(text) is not None:
         message = text
     else:
@@ -31,18 +31,18 @@ def title(soup):
 
 
 def title_lenth(soup):
-    title_text = soup.find('title').getText()
+    title_text = soup.find("title").getText()
     title_length = len(title_text)
     return title_length
 
 
 def meta_description(soup):
-    message = 'No Meta Description Found'
+    message = "No Meta Description Found"
 
-    content1 = soup.find('meta', {'name': 'description'})
-    content2 = soup.find('meta', {'property': 'og:description'})
+    content1 = soup.find("meta", {"name": "description"})
+    content2 = soup.find("meta", {"property": "og:description"})
     if content2 is not None:
-        message = content2.get('content')
+        message = content2.get("content")
     elif content1 is not None:
         message = content1.get("content")
     else:
@@ -52,10 +52,10 @@ def meta_description(soup):
 
 def meta_description_length(soup):
     length = 0
-    content1 = soup.find('meta', {'name': 'description'})
-    content2 = soup.find('meta', {'property': 'og:description'})
+    content1 = soup.find("meta", {"name": "description"})
+    content2 = soup.find("meta", {"property": "og:description"})
     if content2 is not None:
-        length = len(content2.get('content'))
+        length = len(content2.get("content"))
     elif content1 is not None:
         length = len(content1.get("content"))
     else:
@@ -65,7 +65,7 @@ def meta_description_length(soup):
 
 def h1(soup):
     h1 = []
-    head1 = soup.find_all('h1')
+    head1 = soup.find_all("h1")
     for i in head1:
         h1.append(i.getText())
     return h1
@@ -73,7 +73,7 @@ def h1(soup):
 
 def h2(soup):
     h2 = []
-    head2 = soup.find_all('h2')
+    head2 = soup.find_all("h2")
     for j in head2:
         h2.append(j.getText())
     return h2
@@ -83,8 +83,8 @@ def site(url):
     sitemaps_list = []
     page = f"{url}/sitemap.xml"
     site = requests.get(page)
-    sitemap_index = BeautifulSoup(site.content, 'html.parser')
-    map = sitemap_index.find_all('loc')
+    sitemap_index = BeautifulSoup(site.content, "html.parser")
+    map = sitemap_index.find_all("loc")
     for i in map:
         sitemaps_list.append(i.getText())
     return sitemaps_list
@@ -112,18 +112,18 @@ def error_check(url):
 def check_ssl(url):
     try:
         requests.get(url, verify=True)
-        message = ('VALID SSL certificate!')
+        message = "VALID SSL certificate!"
     except requests.exceptions.SSLError:
-        message = ('INVALID SSL certificate!')
+        message = "INVALID SSL certificate!"
     return message
 
 
 def alt_check(soup):
     notFound = []
-    alt = soup.find_all('img', alt=True)
+    alt = soup.find_all("img", alt=True)
     for img in alt:
-        if len(img['alt']) == 0:
-            notFound.append(img.get('src'))
+        if len(img["alt"]) == 0:
+            notFound.append(img.get("src"))
         else:
             pass
     return notFound
@@ -132,14 +132,14 @@ def alt_check(soup):
 def css_not_minify_check(soup):
     mini = []
     notMini = []
-    link = soup.find_all('link', {'rel': 'stylesheet'})
+    link = soup.find_all("link", {"rel": "stylesheet"})
     # style = soup.find_all('style', {'type': 'text/css'})
     # checking CSS minification
     for i in link:
-        link = i['href']
-        if 'min' in i['href'].split('.'):
+        link = i["href"]
+        if "min" in i["href"].split("."):
             mini.append(link)
-        elif '/*' in i.text:
+        elif "/*" in i.text:
             notMini.append(link)
         else:
             notMini.append(link)
@@ -149,14 +149,14 @@ def css_not_minify_check(soup):
 def css_minify_check(soup):
     mini = []
     notMini = []
-    link = soup.find_all('link', {'rel': 'stylesheet'})
+    link = soup.find_all("link", {"rel": "stylesheet"})
     # style = soup.find_all('style', {'type': 'text/css'})
     # checking CSS minification
     for i in link:
-        link = i['href']
-        if 'min' in i['href'].split('.'):
+        link = i["href"]
+        if "min" in i["href"].split("."):
             mini.append(link)
-        elif '/*' in i.text:
+        elif "/*" in i.text:
             notMini.append(link)
         else:
             notMini.append(link)
@@ -166,12 +166,12 @@ def css_minify_check(soup):
 def JS_minify_check(soup):
     mini = []
     notMini = []
-    script = soup.find_all('script')
+    script = soup.find_all("script")
     for i in script:
-        l = str(i.get('src'))
-        if 'min' not in l.split('.'):
+        l = str(i.get("src"))
+        if "min" not in l.split("."):
             notMini.append(l)
-        elif '\n' in i.getText():
+        elif "\n" in i.getText():
             notMini.append(l)
         else:
             mini.append(l)
@@ -181,12 +181,12 @@ def JS_minify_check(soup):
 def JS_not_minify_check(soup):
     mini = []
     notMini = []
-    script = soup.find_all('script')
+    script = soup.find_all("script")
     for i in script:
-        l = str(i.get('src'))
-        if 'min' not in l.split('.'):
+        l = str(i.get("src"))
+        if "min" not in l.split("."):
             notMini.append(l)
-        elif '\n' in i.getText():
+        elif "\n" in i.getText():
             notMini.append(l)
         else:
             mini.append(l)
@@ -195,7 +195,7 @@ def JS_not_minify_check(soup):
 
 def url_canonicaction_check(url):
     i = str(url.split())
-    if '_' in i:
+    if "_" in i:
         message = "YES"
     else:
         message = "NO"
@@ -203,24 +203,24 @@ def url_canonicaction_check(url):
 
 
 def old_tag_check(soup):
-    old_tags = ['applet', 'font', 'center', 's', 'strike', 'u']
+    old_tags = ["applet", "font", "center", "s", "strike", "u"]
     for i in old_tags:
         tags = soup.find_all(i)
         if i in tags:
             message = {i}
         else:
-            message = 'no issues'
+            message = "no issues"
     return message
 
 
 def get_favicon(soup, url):
     """Scrape favicon."""
     if soup.find("link", attrs={"rel": "icon"}):
-        favicon = soup.find("link", attrs={"rel": "icon"}).get('href')
+        favicon = soup.find("link", attrs={"rel": "icon"}).get("href")
     elif soup.find("link", attrs={"rel": "shortcut icon"}):
-        favicon = soup.find("link", attrs={"rel": "shortcut icon"}).get('href')
+        favicon = soup.find("link", attrs={"rel": "shortcut icon"}).get("href")
     elif soup.find("meta", attrs={"itemprop": "image"}):
-        favicon = soup.find("meta", attrs={"itemprop": "image"}).get('content')
+        favicon = soup.find("meta", attrs={"itemprop": "image"}).get("content")
     else:
         favicon = ""
     return favicon
@@ -228,45 +228,44 @@ def get_favicon(soup, url):
 
 def google_analytics_check(soup):
     script_list = []
-    message = ''
-    scrips = soup.find_all('script')
+    message = ""
+    scrips = soup.find_all("script")
     for i in scrips:
-        script_list.append(str(i.get('src')))
+        script_list.append(str(i.get("src")))
     for i in script_list:
-        if i.startswith('https://www.googletagmanager'):
+        if i.startswith("https://www.googletagmanager"):
             message = i
             break
     else:
-        message = 'Not connected to Google Analytics'
+        message = "Not connected to Google Analytics"
     return message
 
 
 def page_load_time(url):
 
     options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
-    driver = webdriver.Chrome()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+
+    # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    # driver = webdriver.Chrome()
 
     # options.binary_location = '/usr/bin/google-chrome'
     # driver = webdriver.Chrome(service=ChromeService('/usr/bin/chromedriver'), options=options)
 
-    # folder = pathlib.Path(__file__).parent.resolve()
-    # chrome_driver_path = str(folder) + "\chromedriver.exe"
-    # driver = webdriver.Chrome(chrome_driver_path, options=options)
+    chrome_driver_path = r'/home/dimitri/api-outstrip-seo/myapp/chromedriver.exe'
+    driver = webdriver.Chrome(chrome_driver_path, options=options)
 
     driver.get(url)
     # navigationStart = driver.execute_script("return window.performance.timing.navigationStart")
     responseStart = driver.execute_script(
-        "return window.performance.timing.responseStart")
-    domComplete = driver.execute_script(
-        "return window.performance.timing.domComplete")
+        "return window.performance.timing.responseStart"
+    )
+    domComplete = driver.execute_script("return window.performance.timing.domComplete")
 
     # backendPerformance_calc = responseStart - navigationStart
-    load_time = round((domComplete - responseStart)/1000, 2)
+    load_time = round((domComplete - responseStart) / 1000, 2)
     driver.quit()
     return load_time
 
@@ -276,84 +275,91 @@ def screenshot(url):
     ob = Screenshot.Screenshot()
     # driver = webdriver.Chrome()
     options = webdriver.ChromeOptions()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(
+        service=ChromeService(ChromeDriverManager().install()), options=options
+    )
     driver.get(url)
-    img_url = ob.full_screenshot(driver, save_path='./media', image_name='screenshot.png', is_load_at_runtime=True,
-                                          load_wait_time=3)
+    img_url = ob.full_screenshot(
+        driver,
+        save_path="./media",
+        image_name="screenshot.png",
+        is_load_at_runtime=True,
+        load_wait_time=3,
+    )
     print(img_url)
     driver.close()
     driver.quit()
-    
+
     return img_url
 
 
 def cdn_delivered(soup):
     found = []
     notFound = []
-    images = soup.find_all('img')
-    link = soup.find_all('link', {'rel': 'stylesheet'})
-    script = soup.find_all('script')
+    images = soup.find_all("img")
+    link = soup.find_all("link", {"rel": "stylesheet"})
+    script = soup.find_all("script")
     for img in images:
-        I = str(img.get('src'))
-        if I.startswith('https://cdn'):
+        I = str(img.get("src"))
+        if I.startswith("https://cdn"):
             found.append(I)
         else:
             notFound.append(I)
     for i in link:
-        J = i['href']
-        if J.startswith('https://cdn'):
+        J = i["href"]
+        if J.startswith("https://cdn"):
             found.append(J)
         else:
             notFound.append(J)
     for i in script:
-        l = str(i.get('src'))
-        if l.startswith('https://cdn'):
-            found.append(i.get('src'))
+        l = str(i.get("src"))
+        if l.startswith("https://cdn"):
+            found.append(i.get("src"))
         else:
-            notFound.append(i.get('src'))
+            notFound.append(i.get("src"))
     return found
 
 
 def cdn_not_delivered(soup):
     found = []
     notFound = []
-    images = soup.find_all('img')
-    link = soup.find_all('link', {'rel': 'stylesheet'})
-    script = soup.find_all('script')
+    images = soup.find_all("img")
+    link = soup.find_all("link", {"rel": "stylesheet"})
+    script = soup.find_all("script")
     for img in images:
-        I = str(img.get('src'))
-        if I.startswith('https://cdn'):
+        I = str(img.get("src"))
+        if I.startswith("https://cdn"):
             found.append(I)
         else:
             notFound.append(I)
     for i in link:
-        J = i['href']
-        if J.startswith('https://cdn'):
+        J = i["href"]
+        if J.startswith("https://cdn"):
             found.append(J)
         else:
             notFound.append(J)
     for i in script:
-        l = str(i.get('src'))
-        if l.startswith('https://cdn'):
-            found.append(i.get('src'))
+        l = str(i.get("src"))
+        if l.startswith("https://cdn"):
+            found.append(i.get("src"))
         else:
-            notFound.append(i.get('src'))
+            notFound.append(i.get("src"))
     return notFound
 
 
 def responsive_image(soup):
     src = []
-    images = soup.find_all('img')
+    images = soup.find_all("img")
     for img in images:
         i = str(img)
         try:
-            if 'srcset' in i:
-                src.append(img.get('src'))
-            elif 'sizes=' in i:
-                src.append(img.get('src'))
+            if "srcset" in i:
+                src.append(img.get("src"))
+            elif "sizes=" in i:
+                src.append(img.get("src"))
         except:
             pass
     return src
@@ -361,14 +367,14 @@ def responsive_image(soup):
 
 def non_responsive_image(soup):
     notFound = []
-    images = soup.find_all('img')
+    images = soup.find_all("img")
     for img in images:
         i = str(img)
         try:
-            if 'srcset' not in i:
-                notFound.append(img.get('src'))
+            if "srcset" not in i:
+                notFound.append(img.get("src"))
             elif 'srcset=""' in i:
-                notFound.append(img.get('src'))
+                notFound.append(img.get("src"))
         except:
             pass
     return notFound
@@ -377,7 +383,7 @@ def non_responsive_image(soup):
 def score(soup, url):
     counter = 100
     title_content = title(soup)
-    if title_content == 'No Title Found':
+    if title_content == "No Title Found":
         counter -= 5
     else:
         pass
@@ -387,7 +393,7 @@ def score(soup, url):
     else:
         counter -= 5
     mt_des_content = meta_description(soup)
-    if mt_des_content == 'No Meta Description Found':
+    if mt_des_content == "No Meta Description Found":
         counter -= 5
     else:
         pass
@@ -422,7 +428,7 @@ def score(soup, url):
     else:
         pass
     ssl = check_ssl(url)
-    if ssl == 'INVALID SSL certificate!':
+    if ssl == "INVALID SSL certificate!":
         counter -= 5
     else:
         pass
@@ -447,7 +453,7 @@ def score(soup, url):
     else:
         pass
     old_tag = old_tag_check(soup)
-    if old_tag != 'no issues':
+    if old_tag != "no issues":
         counter -= 5
     else:
         pass
@@ -457,7 +463,7 @@ def score(soup, url):
     else:
         pass
     google_analytics = google_analytics_check(soup)
-    if google_analytics != 'Not connected to Google Analytics':
+    if google_analytics != "Not connected to Google Analytics":
         counter -= 5
     else:
         pass
@@ -480,27 +486,27 @@ def score(soup, url):
 
 
 def index(request):
-    return render(request, 'index.html')
+    return render(request, "index.html")
 
 
 def is_seo_friendly(url):
-    
+
     # Remove protocol and www prefix from the URL
-    url = re.sub(r'^https?://(www\.)?', '', url)
-    
+    url = re.sub(r"^https?://(www\.)?", "", url)
+
     # Match against common SEO-friendly URL patterns
     patterns = [
-        r'^[a-z0-9-]+$',  # Only lowercase letters, numbers, and hyphens
-        r'^[a-z0-9-]+-[a-z0-9-]+$',  # Two or more words separated by hyphens
-        r'^[a-z0-9-]+/[a-z0-9-]+$',  # Two or more words separated by a slash
+        r"^[a-z0-9-]+$",  # Only lowercase letters, numbers, and hyphens
+        r"^[a-z0-9-]+-[a-z0-9-]+$",  # Two or more words separated by hyphens
+        r"^[a-z0-9-]+/[a-z0-9-]+$",  # Two or more words separated by a slash
     ]
-    
+
     for pattern in patterns:
         if re.match(pattern, url):
             return True
         else:
             return False
-    
+
 
 def check_social_networks(url):
     # Fetch the HTML content of the web page
@@ -508,13 +514,13 @@ def check_social_networks(url):
     html_content = response.text
 
     # Define a list of popular social networks
-    social_networks = ['facebook', 'twitter', 'instagram', 'linkedin']
+    social_networks = ["facebook", "twitter", "instagram", "linkedin"]
 
     # Check if the web page is connected to any of the social networks
     connected_networks = []
     for network in social_networks:
         # Search for the social media URLs in the HTML content
-        pattern = r'(?:https?:\/\/)?(?:www\.)?{0}\.com'.format(network)
+        pattern = r"(?:https?:\/\/)?(?:www\.)?{0}\.com".format(network)
         match = re.search(pattern, html_content, re.IGNORECASE)
         if match:
             connected_networks.append(network)
