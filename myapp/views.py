d733784bd8fd9c830451edfd48d472bf7b6cf8f4
Dimitri NSD <it@tenteeglobal.com>
2024-02-15 08:23:41 +0100

    fixing deployment bugs (34)

--- myapp/views.py
+++ myapp/views.py
@@ -244,14 +244,17 @@ def google_analytics_check(soup):
 def page_load_time(url):
 
     options = Options()
-    options.add_argument('--headless')
-    options.add_argument('--no-sandbox')
-    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument("--headless")
+    options.add_argument("--disable-gpu")
+    options.add_argument("--no-sandbox")
+    options.add_argument("enable-automation")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-dev-shm-usage")
     options.binary_location = '/usr/bin/google-chrome'
 
-    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    # driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
 
-    # driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome(options=options)
 
     driver.get(url)
     # navigationStart = driver.execute_script("return window.performance.timing.navigationStart")
